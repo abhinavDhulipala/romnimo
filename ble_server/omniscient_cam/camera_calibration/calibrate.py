0109215dac514ecd478e506e00525657e7a0e871
mhallac <43485457+mhallac@users.noreply.github.com>
2021-12-17 23:14:16 -0800

    Update calibrate.py

--- ble_server/omniscient_cam/camera_calibration/calibrate.py
+++ ble_server/omniscient_cam/camera_calibration/calibrate.py
@@ -3,6 +3,7 @@ import cv2
 import glob
 import yaml
 
+# Camera calibration comes from here: https://nikatsanka.github.io/camera-calibration-using-opencv-and-python.html
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((7*7,3), np.float32)
